47ebe72886f817fb789f1f9b4f3e71a529a6a5c5
Nick Ivanov <nick@nnbits.org>
2017-03-13 17:42:34 -0500

    3/12/2017

---
+++ src/CSVCandleReader.java
@@ -0,0 +1,32 @@
+/****************************************************************************
+ *    sm230 -- Non-traditional Forex Research Tool
+ *    Copyright (C) 2017  Nick Ivanov
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    E-mail: nick@nnbits.org
+ *    Website: http://nnbits.org/sm230
+ *****************************************************************************/
+
+public class CSVCandleReader {
+    private String filename;
+
+    public CSVCandleReader(String filename) {
+        this.filename = filename;
+    }
+
+    public SM230Candle nextCandle() throws Exception {
+        return null;
+    }
+}
